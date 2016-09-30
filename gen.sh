cd2b801f3a0a0bcbb5a0c27e79b7e345bdc7a32c
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2016-09-30 23:19:55 +0200

    Make builds more deterministic
    ghc-core produced different results in the second run, apparently
    because of the output left over from the first run, in particular the
    leftover interface file `BenchSum.hi`.

--- gen.sh
+++ gen.sh
@@ -8,12 +8,18 @@ sayDo() {
     $1 2>&1 | tee -a $out
 }
 
+clean() {
+    rm -f BenchSum.hi
+}
+
 for resolver in 2.22 6.19 7.0; do
     # Prevent stack from reusing benchmarks built for another GHC.
     rm -rf .stack-work
     stack --resolver lts-$resolver build --bench --no-run-benchmarks
     sayDo "stack --resolver lts-$resolver exec -- ghc-core --no-syntax -- -O BenchSum.hs" BenchSum-lts-$resolver.corehs
+    clean
     sayDo "stack --resolver lts-$resolver exec -- ghc-core -- -O BenchSum.hs" BenchSum-lts-$resolver-color.corehs
+    clean
     sayDo "stack --resolver lts-$resolver bench" Zoutput-lts-$resolver.txt
 done
 
