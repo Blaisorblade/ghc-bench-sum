5bb25d3e6c75f19f7680dccef7a97d6257314137
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2016-09-30 21:54:01 +0200

    Add output generator

---
+++ gen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+outDir=compiler-outs
+
+sayDo() {
+    out=$outDir/$2
+    echo '$' $1 > $out
+    $1 2>&1 | tee -a $out
+}
+
+for resolver in 2.22 6.19 7.0; do
+    ghcPath=$(stack --resolver lts-$resolver exec which ghc)
+    sayDo "stack --resolver lts-$resolver exec -- ghc-core -w $ghcPath --no-syntax -- -O BenchSum.hs" BenchSum-lts-$resolver.corehs
+    sayDo "stack --resolver lts-$resolver exec -- ghc-core -w $ghcPath -- -O BenchSum.hs" BenchSum-lts-$resolver-color.corehs
+    sayDo "stack bench" Zoutput-lts-$resolver.txt
+done
