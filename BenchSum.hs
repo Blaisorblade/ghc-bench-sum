ce96ce6c4bf6481ead6a9db040ef45d262eb1892
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2016-09-30 21:24:05 +0200

    Allow running on GHC 7.8.4 as well

--- BenchSum.hs
+++ BenchSum.hs
@@ -1,16 +1,24 @@
 import Criterion.Main
+import qualified Data.Foldable as F
+import Prelude
+import qualified Prelude as P
 
 myList :: [Int]
 myList = [1, 2 .. 10000]
 
 --sum2 :: [Int] -> Int
--- Ten times faster than sum. FFS?
-sum2 :: (Foldable t, Num a) => t a -> a
-sum2 xs = sum xs
+-- Ten times faster than sum on GHC 7.10.3 and 8.0.1. FFS?
+fsum2 :: (F.Foldable t, Num a) => t a -> a
+fsum2 xs = F.sum xs
+
+psum2 :: (Num a) => [a] -> a
+psum2 xs = P.sum xs
 
 main :: IO ()
 main = defaultMain $
-  [ bgroup "avgSimpl" [ bench "sum2" $ nf sum2 myList
-                      , bench "sum" $ nf sum myList
+  [ bgroup "avgSimpl" [ bench "fsum2" $ nf fsum2 myList
+                      , bench "F.sum" $ nf F.sum myList
+                      , bench "psum2" $ nf psum2 myList
+                      , bench "P.sum" $ nf P.sum myList
                       ]
   ]
