194355ba45d61243bc8b9c5df03dfee8d0c14eff
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2016-10-01 00:24:44 +0200

    Update source and outputs

--- BenchSum.hs
+++ BenchSum.hs
@@ -6,19 +6,24 @@ import qualified Prelude as P
 myList :: [Int]
 myList = [1, 2 .. 10000]
 
---sum2 :: [Int] -> Int
 -- Ten times faster than sum on GHC 7.10.3 and 8.0.1. FFS?
-fsum2 :: (F.Foldable t, Num a) => t a -> a
-fsum2 xs = F.sum xs
+fsum1 :: (F.Foldable t, Num a) => t a -> a
+fsum1 xs = F.foldl (+) 0 xs
 
-psum2 :: (Num a) => [a] -> a
-psum2 xs = P.sum xs
+fsum0 :: (F.Foldable t, Num a) => t a -> a
+fsum0 = F.foldl (+) 0
+
+psum1 :: (Num a) => [a] -> a
+psum1 xs = P.foldl (+) 0 xs
+
+psum0 :: (Num a) => [a] -> a
+psum0 = P.foldl (+) 0
 
 main :: IO ()
 main = defaultMain $
-  [ bgroup "avgSimpl" [ bench "fsum2" $ nf fsum2 myList
-                      , bench "F.sum" $ nf F.sum myList
-                      , bench "psum2" $ nf psum2 myList
-                      , bench "P.sum" $ nf P.sum myList
+  [ bgroup "avgSimpl" [ bench "fsum2" $ nf fsum1 myList
+                      , bench "fsum1" $ nf fsum0 myList
+                      , bench "psum2" $ nf psum1 myList
+                      , bench "P.sum" $ nf psum0 myList
                       ]
   ]
